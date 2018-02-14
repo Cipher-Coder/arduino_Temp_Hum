915781510c701508794b506fca2a6ea6eec51ec8
Cipher-Coder <matthew.rebehn@gmail.com>
2018-02-14 00:05:16 -0500

    Initial Commit

---
+++ temp_Hum/Release/sloeber.ino.cpp.d
@@ -0,0 +1,46 @@
+sloeber.ino.cpp.o: ../sloeber.ino.cpp \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/binary.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/WCharacter.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/WString.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/HardwareSerial.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Stream.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Print.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Printable.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/USBAPI.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/variants/standard/pins_arduino.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/libraries/LiquidCrystal/1.0.7/src/LiquidCrystal.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Print.h \
+ /home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/libraries/DHT_sensor_library/1.3.0/DHT.h \
+ ../temp_Hum.ino
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/binary.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/WCharacter.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/WString.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/HardwareSerial.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Stream.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Print.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Printable.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/USBAPI.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/variants/standard/pins_arduino.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/libraries/LiquidCrystal/1.0.7/src/LiquidCrystal.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Print.h:
+
+/home/matthew/eclipse/java-oxygen/eclipse/arduinoPlugin/libraries/DHT_sensor_library/1.3.0/DHT.h:
+
+../temp_Hum.ino:
---
+++ temp_Hum/temp_Hum.ino
@@ -0,0 +1,41 @@
+#include "Arduino.h"
+#include "LiquidCrystal.h"
+#include "DHT.h"
+//The setup function is called once at startup of the sketch
+
+//Set the DHT Pin
+#define DHTPIN 8
+//Initialize the Library witht he numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+//Setup the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  dht.begin();
+//Print a message to the LCD
+  lcd.print("Temp:  Humidity:");
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+  delay(500);
+//Set cursor to column 0, line 1(actually 2nd row)
+  lcd.setCursor(0, 1);
+  //Read humidity
+  float h = dht.readHumidity();
+  //read temp in fahrenheit
+  float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(f)) {
+	  lcd.print("ERROR");
+	  return;
+  }
+  lcd.print(f);
+  lcd.setCursor(7, 1);
+  lcd.print(h);
+
+}
